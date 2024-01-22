1caf9a31dbe2d6284ce7fbf673ef5411fb8124d1
chonthichar <chonthichar.soythong@gmail.com>
2024-01-22 09:21:13 +0100

    change in dist index and vite file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://chonthichar.github.io/Map-Base---2/",
+    // base: "https://chonthichar.github.io/Map-Base---2/",
 });
