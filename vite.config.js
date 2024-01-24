7ba6def7770e7599e61020dc44705b0da012dfa5
chonthichar <chonthichar.soythong@gmail.com>
2024-01-24 14:19:07 +0100

    adjust on vite2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/Map-Base---2/tree/master/"
+    base: "/Map-Base---2/"
 })
