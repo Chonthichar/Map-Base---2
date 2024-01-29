c54a4ba4451c4c59a45b210ee1af55e2093ad74c
chonthichar <chonthichar.soythong@gmail.com>
2024-01-29 12:49:56 +0100

    add map

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, {MutableRefObject, useEffect, useRef} from "react";
 import ReactDOM from "react-dom/client";
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello React WOrld</h1>);
+
+import "./components/application/application.css"
+
+const map = new Map({
+    layers: [
+        new TileLayer({source: new OSM()})
+    ],
+    view: new View({
+        center: [11, 59], zoom: 5
+    })
+})
+
+function MapApplication() {
+    const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+    useEffect(() => {
+        map.setTarget(mapRef.current);
+    }, []);
+    return <div ref={mapRef} style={{width: '100%', height: '100vh'}}></div>;
+}
+
+root.render(<MapApplication/>)
